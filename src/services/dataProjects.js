49be57002894fe4db70525867bf7738481afba88
Anadbh <anafruiz20@gmail.com>
2021-05-10 11:29:27 +0200

    Update project quick quiz game

--- src/services/dataProjects.js
+++ src/services/dataProjects.js
@@ -6,45 +6,53 @@ import project3 from "../images/project3.png";
 import project4 from "../images/project4.png";
 import project5 from "../images/project5.png";
 import project6 from "../images/project6.png";
+import project7 from "../images/project7.png";
 
 const dataProjects = [
   {
     id: 1,
+    url: "https://anafruiz.github.io/quick-quiz/",
+    img: project7,
+    name: "JavaScript",
+    github: "https://github.com/Anafruiz/quick-quiz",
+  },
+  {
+    id: 2,
     url: "https://anafruiz.github.io/Rick-Morty-character-finder/#/",
     img: project1,
     name: "React",
     github: "https://github.com/Anafruiz/Rick-Morty-character-finder",
   },
   {
-    id: 2,
+    id: 3,
     url: "https://anafruiz.github.io/tv-series-searcher/",
     img: project2,
     name: "JavaScript",
     github: "https://github.com/Anafruiz/tv-series-searcher",
   },
   {
-    id: 3,
+    id: 4,
     url: "https://anafruiz-react-magicians.herokuapp.com/#/",
     img: project3,
     name: "React & Node JS",
     github: "https://github.com/Anafruiz/project-promo-l-module-3-team-1",
   },
   {
-    id: 4,
+    id: 5,
     url: "https://anafruiz.github.io/Local-Storage-Tapas/",
     img: project6,
     name: "JavaScript",
     github: "https://github.com/Anafruiz/Local-Storage-Tapas",
   },
   {
-    id: 5,
+    id: 6,
     url: "https://anafruiz.github.io/Guess-a-number/",
     img: project4,
     name: "JavaScript",
     github: "https://github.com/Anafruiz/Guess-a-number",
   },
   {
-    id: 6,
+    id: 7,
     url: "https://anafruiz.github.io/Pokemon-React/",
     img: project5,
     name: "React",
