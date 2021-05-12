9315b8a9faa4def4bd009b16f3881571b7e89889
Anadbh <anafruiz20@gmail.com>
2021-05-12 12:21:32 +0200

    Update image seo

--- src/services/dataProjects.js
+++ src/services/dataProjects.js
@@ -1,12 +1,12 @@
-import project1 from "../images/project1.png";
-import project2 from "../images/project2.png";
+import project1 from "../images/project1.jpg";
+import project2 from "../images/project2.jpg";
 
-import project3 from "../images/project3.png";
+import project3 from "../images/project3.jpg";
 
-import project4 from "../images/project4.png";
-import project5 from "../images/project5.png";
-import project6 from "../images/project6.png";
-import project7 from "../images/project7.png";
+import project4 from "../images/project4.jpg";
+import project5 from "../images/project5.jpg";
+import project6 from "../images/project6.jpg";
+import project7 from "../images/project7.jpg";
 
 const dataProjects = [
   {
